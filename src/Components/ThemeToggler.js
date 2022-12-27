26841e56daa9ff8ba33514051fdfea448679bf51
anshulghogre4 <anshulghogre4@gmail.com>
2022-12-28 00:50:34 +0530

    Added logic for the toggle between light& dark

---
+++ src/Components/ThemeToggler.js
@@ -0,0 +1,20 @@
+import React,{useContext, useState} from "react";
+import ThemeContext from "../Context/ThemeContext";
+
+
+const ThemeToggler = ()=> {
+
+const [themeMode, setThemeMode] = useContext(ThemeContext)
+
+    return(
+        <div onClick={()=>{
+            setThemeMode(themeMode === "light"? "dark":"light" ) 
+        }}>
+            <p>{themeMode === "light"? "dark mode":"light mode"}</p>
+        </div>
+    )
+
+
+}
+
+export default ThemeToggler;
