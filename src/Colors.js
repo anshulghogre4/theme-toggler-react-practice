c4c8ba9d9596e3eb0da59a25e0f001be1793753a
anshulghogre4 <anshulghogre4@gmail.com>
2022-12-28 00:05:08 +0530

    added light and dark objects in Colors.js file

---
+++ src/Colors.js
@@ -0,0 +1,14 @@
+const AppTheme  = {
+    light:{
+        textColor:"#000000",
+        backgroundColor: "#ffffff",
+        border: "solid 2px #000000"
+    },
+    dark:{
+        textColor:"#ffffff",
+        backgroundColor: "#313131",
+        border: "dashed 2px #ffffff"
+    }
+}
+
+export default AppTheme;
